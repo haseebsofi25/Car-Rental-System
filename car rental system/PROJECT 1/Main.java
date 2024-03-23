c6fad5c5b56c32486da96028b295e05f20a3ea26
haseebsofi25 <haseebsofi25@gmail.com>
2024-03-23 20:00:21 +0530

    added files

--- car rental system/PROJECT 1/Main.java
+++ car rental system/PROJECT 1/Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+//Stores Car Details
 class Car {
     private String carId;
     private String brand;
@@ -9,13 +10,17 @@ class Car {
     private double basePricePerDay;
     private boolean isAvailable;
 
-    public Car(String carId, String brand, String model, double basePricePerDay) {
+    //Constructors: To initialize the object in the class And to return value
+    public Car(String carId, String brand, String model, double basePricePerDay) { // this is used as current obj (to insert values into specific current working objs)
         this.carId = carId;
         this.brand = brand;
         this.model = model;
         this.basePricePerDay = basePricePerDay;
         this.isAvailable = true;
+        //We use true because it is a boolean value
+
     }
+    // to Get the values stored
     public String getCarId() {
         return carId;
     }
@@ -45,6 +50,7 @@ class Car {
     }
 }
 
+//Stores Customer Details
 class Customer {
     private String customerId;
     private String name;
@@ -63,10 +69,12 @@ class Customer {
     }
 }
 
+//Combines both customer and car class. Stores values such as which customer takes which car
 class Rental {
-    private Car car;
+    //Class is a type of data type not primitive tho, object is type of class
+    private Car car; //Here car is the variable of Car class
     private Customer customer;
-    private int days;
+    private int days; //no. of days taken for rent
 
     public Rental(Car car, Customer customer, int days) {
         this.car = car;
@@ -87,17 +95,20 @@ class Rental {
     }
 }
 
+//For inputing and storing Cars
 class CarRentalSystem {
-    private List<Car> cars;
+    private List<Car> cars; //List to store car data
     private List<Customer> customers;
-    private List<Rental> rentals;
+    private List<Rental> rentals; //Car rented by the customer data
 
+    //We built a constructor to store the list in memory
     public CarRentalSystem() {
         cars = new ArrayList<>();
         customers = new ArrayList<>();
         rentals = new ArrayList<>();
     }
 
+    //.add - adds a value to the array list. appends the value to the last.
     public void addCar(Car car) {
         cars.add(car);
     }
@@ -106,22 +117,24 @@ class CarRentalSystem {
         customers.add(customer);
     }
 
+    //Stores both customer and cars details along with rental days and checks whether the car is available for renting.
     public void rentCar(Car car, Customer customer, int days) {
         if (car.isAvailable()) {
-            car.rent();
-            rentals.add(new Rental(car, customer, days));
+            car.rent(); //Calls rent() function
+            rentals.add(new Rental(car, customer, days)); //adds the car for renting to the list
 
         } else {
             System.out.println("Car is not available for rent.");
         }
     }
 
+    //For Returning the car
     public void returnCar(Car car) {
-        car.returnCar();
-        Rental rentalToRemove = null;
+        car.returnCar(); //calls the return car function through which we get to know that car is available for rent
+        Rental rentalToRemove = null; //Here we make Rental variable of rental class and use loops to iterate it
         for (Rental rental : rentals) {
-            if (rental.getCar() == car) {
-                rentalToRemove = rental;
+            if (rental.getCar() == car) { //getcar() will return us the car and check if the car is available or not.
+                rentalToRemove = rental; // here we know that the customer has returned the car or not
                 break;
             }
         }
@@ -133,10 +146,11 @@ class CarRentalSystem {
         }
     }
 
+    //In the end we make an interface for the user to Select the car he wants to rent.
     public void menu() {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); //here we take input from the user
 
-        while (true) {
+        while (true) { //the menu will work until true i.e until not exited
             System.out.println("===== Car Rental System =====");
             System.out.println("1. Rent a Car");
             System.out.println("2. Return a Car");
@@ -150,6 +164,7 @@ class CarRentalSystem {
                 System.out.println("\n== Rent a Car ==\n");
                 System.out.print("Enter your name: ");
                 String customerName = scanner.nextLine();
+                //We create a variable customerName and store details in that
 
                 System.out.println("\nAvailable Cars:");
                 for (Car car : cars) {
@@ -166,8 +181,10 @@ class CarRentalSystem {
                 scanner.nextLine(); // Consume newline
 
                 Customer newCustomer = new Customer("CUS" + (customers.size() + 1), customerName);
+                //(customers.size() + 1) shows id in numerical order
                 addCustomer(newCustomer);
 
+                //Now we check if the car is available or not
                 Car selectedCar = null;
                 for (Car car : cars) {
                     if (car.getCarId().equals(carId) && car.isAvailable()) {
@@ -178,6 +195,7 @@ class CarRentalSystem {
 
                 if (selectedCar != null) {
                     double totalPrice = selectedCar.calculatePrice(rentalDays);
+                    //Here we check the total price for renting
                     System.out.println("\n== Rental Information ==\n");
                     System.out.println("Customer ID: " + newCustomer.getCustomerId());
                     System.out.println("Customer Name: " + newCustomer.getName());
@@ -189,6 +207,7 @@ class CarRentalSystem {
                     String confirm = scanner.nextLine();
 
                     if (confirm.equalsIgnoreCase("Y")) {
+                        //equalsIgnoreCase - ignores case senstivity
                         rentCar(selectedCar, newCustomer, rentalDays);
                         System.out.println("\nCar rented successfully.");
                     } else {
@@ -242,10 +261,12 @@ class CarRentalSystem {
 public class Main{
     public static void main(String[] args) {
         CarRentalSystem rentalSystem = new CarRentalSystem();
+        //First we make an object for the CarRentalSystem class
 
-        Car car1 = new Car("C001", "Toyota", "Camry", 60.0); // Different base price per day for each car
-        Car car2 = new Car("C002", "Honda", "Accord", 70.0);
-        Car car3 = new Car("C003", "Mahindra", "Thar", 150.0);
+
+        Car car1 = new Car("C001", "Toyota", "Land Cruiser", 90.0); // Different base price per day for each car
+        Car car2 = new Car("C002", "Nissan", "Patrol", 100.0);
+        Car car3 = new Car("C003", "Porche", "911", 150.0);
         rentalSystem.addCar(car1);
         rentalSystem.addCar(car2);
         rentalSystem.addCar(car3);
